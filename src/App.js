3ba08e7515069f713071f301a2b18cb0f926cd4f
Ed Cabiltes <edcabiltes@outlook.com>
2025-01-27 10:25:22 +0800

    Added forms with actions for handling submissions

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import Home from "./pages/Home"
 import About from "./pages/About"
 import HelpLayout from "./layouts/HelpLayout"
 import Faq from "./pages/help/Faq"
-import Contact from "./pages/help/Contact"
+import Contact, { contactAction } from "./pages/help/Contact"
 import NotFound from "./pages/NotFound"
 import Careers, { careersLoader } from "./pages/careers/Careers"
 import CareersLayout from "./layouts/CareersLayout"
@@ -27,7 +27,7 @@ const router = createBrowserRouter(
       <Route path="about" element={<About />} />
       <Route path="help" element={<HelpLayout />}>
         <Route path="faq" element={<Faq />} />
-        <Route path="contact" element={<Contact />} />
+        <Route path="contact" element={<Contact />} action={contactAction} />
       </Route>
       <Route path="careers" element={<CareersLayout />} errorElement={<CareersError/>}>
         <Route 
--- src/pages/help/Contact.js
+++ src/pages/help/Contact.js
@@ -1,8 +1,12 @@
+import { Form, redirect, useActionData } from "react-router-dom";
+
 export default function Contact() {
+    const data = useActionData();
+
     return (
         <div className="contact">
             <h3>Contact Us</h3>
-            <form>
+            <Form method="post" action="/help/contact">
                 <label>
                     <span>Email:</span>
                     <input type="email" name="email" required />
@@ -12,8 +16,32 @@ export default function Contact() {
                     <textarea name="message" required></textarea>
                 </label>
                 <button>Send</button>
-            </form>
+
+                {data && data.error && <p>{data.error}</p>}
+            </Form>
         </div>
             
     )
+}
+
+export const contactAction = async ({ request }) => {
+    const data = await request.formData();
+
+    const submission = {
+        email: data.get("email"),
+        message: data.get("message")
+    }
+
+    console.log(submission);
+
+    // send your post request
+
+    if (submission.message.length < 10) {
+        return {
+            error: "Message must be at least 10 characters long"
+        }
+    }
+
+    // redirect the user to the home page
+    return redirect("/");
 }
